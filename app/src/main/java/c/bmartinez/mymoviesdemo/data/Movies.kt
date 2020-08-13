06bbfd09073bd10ee8540040f52c9e8900caee1f
Brian Martinez <brianrmartinezcastaneda@csumb.edu>
2020-08-12 19:35:44 -0700

    Implemented Data class for Movies

---
+++ app/src/main/java/c/bmartinez/mymoviesdemo/data/Movies.kt
@@ -0,0 +1,24 @@
+package c.bmartinez.mymoviesdemo.data
+
+import com.google.gson.annotations.SerializedName
+
+data class Movies(
+    @SerializedName("id")
+    val id: String,
+    @SerializedName("title")
+    val title: String,
+    @SerializedName("year")
+    val year: String,
+    @SerializedName("genres")
+    val genres: List<String>,
+    @SerializedName("duration")
+    val duration: String,
+    @SerializedName("releaseDate")
+    val releaseData: String,
+    @SerializedName("storyline")
+    val storyLine: String,
+    @SerializedName("actors")
+    val actors: List<String>,
+    @SerializedName("posterurl")
+    val posterUrl: String
+)
