ea88b1b453d00e42c5b4fc0a4e0b9aeb12526613
Brian Martinez <brianrmartinezcastaneda@csumb.edu>
2020-08-14 22:09:32 -0700

    Implemented data class for the movies ratings and added the backend request call for the movie ratings data

--- app/src/main/java/c/bmartinez/mymoviesdemo/network/ApiService.kt
+++ app/src/main/java/c/bmartinez/mymoviesdemo/network/ApiService.kt
@@ -1,9 +1,13 @@
 package c.bmartinez.mymoviesdemo.network
 
+import c.bmartinez.mymoviesdemo.data.MovieRatings
 import c.bmartinez.mymoviesdemo.data.Movies
 import retrofit2.http.GET
 
 interface ApiService {
     @GET("/b/5f2c4877dddf413f95be718b")
     suspend fun getMovies(): List<Movies>
+
+    @GET("/b/5f2c4951dddf413f95be721c")
+    suspend fun getMovieRatings(): List<MovieRatings>
 }
