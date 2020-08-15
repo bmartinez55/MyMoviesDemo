4682d3b0e0cb0f13455ea85ca2586976c03fc1fd
Brian Martinez <brianrmartinezcastaneda@csumb.edu>
2020-08-14 22:55:52 -0700

    Implemented Adapter without 'addMovies' function

--- app/src/main/java/c/bmartinez/mymoviesdemo/ui/list/ListAdapter.kt
+++ app/src/main/java/c/bmartinez/mymoviesdemo/ui/list/ListAdapter.kt
@@ -1,29 +1,41 @@
 package c.bmartinez.mymoviesdemo.ui.list
 
 import android.content.Context
+import android.graphics.Movie
+import android.text.method.TextKeyListener.clear
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import c.bmartinez.mymoviesdemo.R
 import c.bmartinez.mymoviesdemo.data.Movies
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.list_item.view.*
+import java.util.Collections.addAll
 
 class ListAdapter(private val movies: List<Movies>): RecyclerView.Adapter<ListAdapter.MovieViewHolder>() {
 
     class MovieViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
         fun bind(movie: Movies){
-
+            itemView.apply {
+                Glide.with(moviePoster.context).load(movie.posterUrl).into(moviePoster)
+            }
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
-        TODO("Not yet implemented")
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder =
+        MovieViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.list_item,parent,false))
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = movies.size
 
     override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(movies[position])
     }
 
+//    fun addMovies(movies: List<Movies>){
+//        this.movies.apply {
+//            clear()
+//            addAll(movies)
+//        }
+//    }
 }
