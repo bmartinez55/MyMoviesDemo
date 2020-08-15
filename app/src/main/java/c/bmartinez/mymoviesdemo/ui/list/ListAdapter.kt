0e12f6b5994aad6388a81f2f0a8959adbdaff1d1
Brian Martinez <brianrmartinezcastaneda@csumb.edu>
2020-08-14 22:48:09 -0700

    Updated class communication with Api calls

--- app/src/main/java/c/bmartinez/mymoviesdemo/ui/list/ListAdapter.kt
+++ app/src/main/java/c/bmartinez/mymoviesdemo/ui/list/ListAdapter.kt
@@ -1,12 +1,17 @@
 package c.bmartinez.mymoviesdemo.ui.list
 
+import android.content.Context
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import c.bmartinez.mymoviesdemo.data.Movies
 
-class ListAdapter: RecyclerView.Adapter<ListAdapter.MovieViewHolder> {
+class ListAdapter(private val movies: List<Movies>): RecyclerView.Adapter<ListAdapter.MovieViewHolder>() {
 
-    class MovieViewHolder(private val movieItemView): RecyclerView.ViewHolder(movieItemView){
+    class MovieViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        fun bind(movie: Movies){
 
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
