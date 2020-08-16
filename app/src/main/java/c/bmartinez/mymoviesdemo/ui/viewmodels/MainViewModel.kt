feabf2dc5e1da6f0d1774abb97a522317dfcd3f1
Brian Martinez <brianrmartinezcastaneda@csumb.edu>
2020-08-15 23:30:18 -0700

    Implemented ViewModel Factory

--- app/src/main/java/c/bmartinez/mymoviesdemo/ui/viewmodels/MainViewModel.kt
+++ app/src/main/java/c/bmartinez/mymoviesdemo/ui/viewmodels/MainViewModel.kt
@@ -3,14 +3,14 @@ package c.bmartinez.mymoviesdemo.ui.viewmodels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
 import c.bmartinez.mymoviesdemo.data.MainRepository
+import c.bmartinez.mymoviesdemo.utils.Resource
 import kotlinx.coroutines.Dispatchers
+import java.lang.Exception
 
-class MainViewModel: ViewModel() {
-    val repository: MainRepository = MainRepository()
+class MainViewModel(private val mainRepository: MainRepository): ViewModel() {
 
-    val getMovies = liveData(Dispatchers.IO){
-        val movieData = repository.getMovies()
-
-        emit(movieData)
+    val getMovies = liveData(Dispatchers.IO) {
+        val data  = mainRepository.getMovies()
+        emit(data)
     }
 }
