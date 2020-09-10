87a4a7c711984bf80f4f438ad4af1e2330ac1215
Brian Martinez <brianrmartinezcastaneda@csumb.edu>
2020-09-09 23:06:32 -0700

    Implemented APIService with Coroutines

--- app/src/main/java/c/bmartinez/mymoviesdemo/network/TmdbApi.kt
+++ app/src/main/java/c/bmartinez/mymoviesdemo/network/TmdbApi.kt
@@ -1,18 +1,12 @@
 package c.bmartinez.mymoviesdemo.network
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import c.bmartinez.mymoviesdemo.data.MovieRatings
-import c.bmartinez.mymoviesdemo.data.Movies
+import c.bmartinez.mymoviesdemo.data.MovieDataResponse
 import kotlinx.coroutines.Deferred
+import retrofit2.Response
 import retrofit2.http.GET
 
 interface TmdbApi {
 
-    //@Headers("user-key: 1c976c5d-e5b0-11ea-b386-0242ac110002")
-    @GET("/")
-    fun getMoviesAsync(): List<Movies>
-
-    @GET("/")
-    fun getMovieRatings(): List<MovieRatings>
+    @GET("movie/popular")
+    suspend fun getMoviesAsync(): Deferred<Response<MovieDataResponse>>
 }
